40be0b5f0aad0c63266402683a16c91f9ae0f860
tscheys <tim.scheys@gmail.com>
2015-09-21 10:51:05 -0700

    Make ajax request to wiki page of charles the fifth

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 $(function() {
+  // data 
+  var content;
+  // search functionality
   $('#search').on('click', function(e) {
     var query = $("#monarch").val();
     console.log('monarch: ' + query);
+  });
+  // ajax request
+  $.ajax({
+      type: "GET",
+      url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Charles_V,_Holy_Roman_Emperor&callback=?",
+      contentType: "application/json; charset=utf-8",
+      async: false,
+      dataType: "json",
+      success: function (data, textStatus, jqXHR) {
+          content = data;
+          console.log(data);
+          $('#content').append('<p>data</p>');
+      },
+      error: function (errorMessage) {
+      }
   });
 });
